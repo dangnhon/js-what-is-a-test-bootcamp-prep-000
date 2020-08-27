30e2db76a65ec9534e7aa2ce8e79e96770b59103
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-27 22:00:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-
+var message2 = `${height} is how tall ${name} is`
 module.exports = {name, height, message}
 console.log(message)
+console.log(message2)
